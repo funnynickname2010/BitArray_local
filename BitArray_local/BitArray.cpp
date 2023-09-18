945ab5cae568ff4f9a13f57f39bf304ff3cc3300
Вячеслав Ремез <badforker@gmail.com>
2023-09-18 10:22:10 +0300

    Constructors done, started BitMask

--- BitArray_local/BitArray.cpp
+++ BitArray_local/BitArray.cpp
@@ -1,1 +1,67 @@
 #include "BitArray.h"
+
+unsigned int BitArray::BitMask(unsigned int& location, int& task)
+{
+	switch (task)
+	{
+	case 1:
+
+	}
+}
+
+BitArray::BitArray()
+{
+	bitarray = new unsigned int(1);
+	used_bits = 0;
+	reserved_ints = 1;
+
+	for (int i = 0; i < reserved_ints; i++)
+	{
+		bitarray[i] = 0;
+	}
+}
+
+BitArray::BitArray(unsigned int& n)
+{
+	bitarray = new unsigned int(n);
+	used_bits = 0;
+	reserved_ints = n;
+
+	for (int i = 0; i < reserved_ints; i++)
+	{
+		bitarray[i] = 0;
+	}
+}
+
+BitArray::BitArray(BitArray& obj2)
+{
+	used_bits = obj2.used_bits;
+	reserved_ints = obj2.reserved_ints;
+	bitarray = new unsigned int(obj2.reserved_ints);
+
+	for (int i = 0; i < reserved_ints; i++)
+	{
+		bitarray[i] = obj2.bitarray[i];
+	}
+}
+
+BitArray::~BitArray()
+{
+	delete[] bitarray;
+}
+
+BitArray BitArray::operator =(BitArray& obj2)
+{
+	if (reserved_ints != obj2.reserved_ints)
+	{
+		reserved_ints = obj2.reserved_ints;
+		bitarray = new unsigned int(obj2.reserved_ints);
+	}
+
+	for (int i = 0; i < reserved_ints; i++)
+	{
+		bitarray[i] = obj2.bitarray[i];
+	}
+
+	used_bits = obj2.used_bits;
+}
--- BitArray_local/BitArray.h
+++ BitArray_local/BitArray.h
@@ -1,5 +1,21 @@
 #pragma once
 class BitArray
 {
+	int unsigned* bitarray;
+	int unsigned reserved_ints;
+	int unsigned used_bits;
+
+	unsigned int BitMask(unsigned int& location, int& task);
+	int ElementIndex();
+
+	void TurnOn(unsigned int& index);
+	void TurnOff(unsigned int& index);
+
+	BitArray operator =(BitArray& obj2);
+
+	BitArray(); //Make a constant
+	BitArray(unsigned int& n);
+	BitArray(BitArray& obj2);
+	~BitArray();
 };
 
